690f6f4665b10f3315fe7f8bdc9500b2220b078d
AramWondergem <aramwondergem@gmail.com>
2024-04-29 18:38:56 -0400

    added a service which collects the csv and shorts them

---
+++ src/app/service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as Papa from "papaparse";
+import {forkJoin, map, Observable} from "rxjs";
+
+
+
+@Injectable({
+  providedIn: 'root'})
+export class ServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getFiles(filePaths: string[]): Observable<any> {
+    const fileObservables: Observable<string[][]>[] = filePaths.map(filePath =>
+      this.http.get(filePath, { responseType: 'text' }).pipe(
+        map(data => Papa.parse(data, { header: false }).data as string[][])
+      )
+    );
+
+    const firstArray: string[][] = [];
+
+    return forkJoin(fileObservables).pipe(
+      map((arrays: string[][][]) => firstArray.concat(...arrays)),
+      map((data: string[][]) => data.sort((a: string[], b: string[]) => a[0].localeCompare(b[0])))
+    );
+  }
+
+
+}
