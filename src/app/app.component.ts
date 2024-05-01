d27757152a03e2fc7d0e1587ad558b17c690741a
AramWondergem <aramwondergem@gmail.com>
2024-05-01 17:01:29 -0400

    added the menu for the mobile function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
+import {NgClass} from "@angular/common";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgClass],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'waterwoorden';
+  menuToggle: boolean = false;
+
+  toggleMenu() {
+    this.menuToggle = !this.menuToggle;
+  }
 
 
 }
