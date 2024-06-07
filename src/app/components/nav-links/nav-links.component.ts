66af80519810a969ee46112a771aa49345db43cb
AramWondergem <aramwondergem@gmail.com>
2024-06-07 15:32:01 -0400

    removed router component

--- src/app/components/nav-links/nav-links.component.ts
+++ src/app/components/nav-links/nav-links.component.ts
@@ -1,25 +1,30 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
 import {NgClass} from "@angular/common";
+import {DataSharingService} from "../../services/data-sharing.service";
 
 @Component({
   selector: 'app-nav-links',
   standalone: true,
   imports: [
-    RouterLinkActive,
-    RouterLink,
     NgClass
   ],
   templateUrl: './nav-links.component.html',
   styleUrl: './nav-links.component.css'
 })
 export class NavLinksComponent {
+  toggle: boolean = true;
 
   @Output() action = new EventEmitter<void>();
   @Input() isFlexColumn: boolean = false;
 
+  constructor(
+    private dataService: DataSharingService
+  ) {
+    this.dataService.toggleNavLinks$.subscribe((value: boolean)  => this.toggle = value)
+  }
+
   onClick() {
-    this.action.emit();
+    this.dataService.toggleNavLinks()
   }
 
 }
